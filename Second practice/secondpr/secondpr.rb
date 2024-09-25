94c3fb9bc1cd180b3631c3258126f610ee41b20e
Maksym Hladkoskok <gladkoskokmaks@gmail.com>
2024-09-25 18:37:09 +0200

    add.gitignore

---
+++ Second practice/secondpr/secondpr.rb
@@ -0,0 +1,47 @@
+def precedence(op)
+  case op
+  when '+', '-'
+    return 1
+  when '*', '/'
+    return 2
+  else
+    return 0
+  end
+end
+
+def is_operator?(c)
+  ['+', '-', '*', '/'].include?(c)
+end
+
+
+def to_rpn(expression)
+  output = []
+  operators = []
+
+
+  tokens = expression.scan(/\d+|[+\-*\/]/)
+
+  tokens.each do |token|
+    if token.match?(/\d+/)
+      output << token
+    elsif is_operator?(token)
+      while !operators.empty? && precedence(operators.last) >= precedence(token)
+        output << operators.pop
+      end
+      operators << token
+    end
+  end
+
+
+  while !operators.empty?
+    output << operators.pop
+  end
+
+  output.join(' ')
+end
+
+
+puts "Enter a mathematical expression:"
+expression = gets.chomp
+
+puts "RPN expression: #{to_rpn(expression)}"
